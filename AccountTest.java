660516b0350987dcc6c8a7f52382bad023a22a05
daus-aglar34 <prabu_aglar_34rpl@student.smktelkom-mlg.sch.id>
2025-08-21 07:20:18 +0700

    Update AccountTest.java

--- AccountTest.java
+++ AccountTest.java
@@ -1,20 +1,23 @@
+import java.util.Scanner;
+
 public class AccountTest {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         SavingsAccount sa0001 = new SavingsAccount();
+        System.out.println("Enter the Deposit Amount:");
+        int depositAmount = input.nextInt();
+
+        System.out.println("Enter the Withdrawal Amount:");
+        int withdrawalAmount = input.nextInt();
         sa0001.balance = 0;
-        sa0001.deposit(5000);
+        sa0001.deposit(depositAmount);
+        sa0001.withdraw(withdrawalAmount);
         sa0001.name = "Damien";
         sa0001.balance += 0.83;
 
-        SavingsAccount sa0002 = new SavingsAccount();
-        sa0002.balance = 2000;
-        sa0002.name = "Bill";
-
-        SavingsAccount sa0003 = new SavingsAccount();
-        sa0003.balance = 3000;
-        sa0003.name = "John";
-
         sa0001.displayCustomer();
         System.out.println(sa0001.calcInterest());
+
+        input.close();
     }
 }
